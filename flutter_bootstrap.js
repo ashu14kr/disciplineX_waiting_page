ba8a7526d3f26cae2303b5eee0ea898aedcc8946
ashu14kr <aasshhuutosh1@gmail.com>
2025-03-17 23:25:37 +0530

    url updates

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"f73bfc4522dd0bc87bbcdb4bb3088082755c5e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "801781813"
+    serviceWorkerVersion: "4231680424"
   }
 });
